c1bda58d76a3d56c2fa308224ba83a213a987bdb
Ali Burkemper <45569410+aliburkemper12@users.noreply.github.com>
2024-05-14 15:07:01 +0000

    added ability to choose which shift to see

--- gr/main.py
+++ gr/main.py
@@ -1,7 +1,14 @@
 import time_check
 
+def get_shift():
+    print('What shift do you want to see?\nType:\n')
+    print('1 - for first\n2 - for second\n3 - for third\n')
+    return input()
+
 def main():
-    
+    print()
+    time_check.do_time_check(shift)
 
 if __name__ == "__main__":
+    shift = get_shift()
     main()
--- gr/time_check.py
+++ gr/time_check.py
@@ -1,192 +1,246 @@
+from datetime import datetime
+import pytz
+
 '''
 program that gets the current local time and tells the user how long it 
 is until first break, second break, clock out, or clock in
 
 '''
-
-from datetime import datetime
-import pytz
-
-# get localized time
-x = pytz.timezone("America/Chicago")
-time = datetime.now(x)
-
-during_work = True
-
-# check if time is before first break, second break, or clock out
-if time.hour < 9 and time.hour >= 6:
-    var = 9 - time.hour
-    var_minutes = 0 - time.minute
+def do_time_check(shift):
+    # print shift name
+    shift_name = get_shift_name(shift)
+    print('------------------', shift_name ,'Shift ------------------\n')
     
-    if var_minutes < 0:
-        var_minutes = 60 + var_minutes
-        var -= 1
-        
-    if var_minutes == 1:
-        if var == 0:
-            print('It is', var_minutes, 'minute until first break')
-        else: 
-            print('It is', var ,'hours and', var_minutes ,'minute until first break')
+    # get localized time
+    x = pytz.timezone("America/Chicago")
+    time = datetime.now(x)
 
-    else:
-        if var == 0:
-            print('It is', var_minutes, 'minutes until first break')
-        else:
-            print('It is', var ,'hours and', var_minutes ,'minutes until first break')
-        
-# it is first break
-elif time.hour == 9 and time.minutes < 15:
-    print('It is currently first break')
+    # boolean that will change if the hours are/aren't within the correct shift period
+    during_work = True
+    
+    # set variables according to shift
+    set_variables(shift)
 
-# after first break before second break
-elif time.hour < 11:
-    # standardize
-    if time.minute > 36:
-        var = 11 - time.hour
-        var_minutes = 36 - time.minute
+    # check if time is before first break, second break, or clock out
+    if time.hour < first_break_hour and time.hour >= start_hour:
+        var = first_break_hour - time.hour
+        var_minutes = 54 - time.minute
         
         if var_minutes < 0:
             var_minutes = 60 + var_minutes
             var -= 1
             
         if var_minutes == 1:
-            print('It is', var ,'hours and', var_minutes ,'minute until lunch')
+            if var == 0:
+                print('It is', var_minutes, 'minute until first break')
+            else: 
+                print('It is', var ,'hours and', var_minutes ,'minute until first break')
 
         else:
-            print('It is', var ,'hours and', var_minutes ,'minutes until lunch')
-           
-    # no need to standardize 
-    else:
-        var = 11 - time.hour
-        var_minutes = 36 - time.minute
-        
-        if var_minutes == 1:
-            print('It is', var ,'hours and', var_minutes ,'minute until lunch')
-
-        else:
-            print('It is', var ,'hours and', var_minutes ,'minutes until lunch')
+            if var == 0:
+                print('It is', var_minutes, 'minutes until first break')
+            else:
+                print('It is', var ,'hours and', var_minutes ,'minutes until first break')
             
-elif time.hour == 11 and time.minutes < 36:
-    var_minutes = 36 - time.minute
-    print('It is', var_minutes ,'minutes until lunch')
-        
-# it is currently lunch time
-elif time.hour == 11 and time.minutes >= 36:
-    print('It is lunch time :)')
-    
-# after lunch before clock out
-elif time.hour < 14:
-     # standardize
-    if time.minute > 30:
-        var = 14 - time.hour
-        var_minutes = 30 - time.minute
-        
-        if var_minutes < 0:
-            var_minutes = 60 + var_minutes
-            var -= 1
-            
-        if var_minutes == 1:
-            print('It is', var ,'hours and', var_minutes ,'minute until lunch')
+    # it is first break
+    elif time.hour == first_break_hour and time.minute < 10:
+        print('It is currently first break')
 
-        else:
-            print('It is', var ,'hours and', var_minutes ,'minutes until lunch')
-           
-    # no need to standardize 
-    else:
-        var = 14 - time.hour
-        var_minutes = 30 - time.minutes
-        
-        if var_minutes == 1:
-            print('It is', var ,'hours and', var_minutes ,'minute until lunch')
-
-        else:
-            print('It is', var ,'hours and', var_minutes ,'minutes until lunch')
-        
-elif time.hour == 14 and time.minutes < 30:
-    var_minutes = time.minute
-    print('It is', var_minutes ,'minutes until clock out')
-    
-# outside of work hours
-else:
-    during_work = False
-    print('You are not currently scheduled!')
-    # if it is before midnight
-    if time.hour >= 14 and time.hour < 0:
-        # add 6 hours and 30 minutes onto difference between curr and midnight
-        var_hours = 24 - time.hour + 6
-        var_minutes = time.minutes
-        
+    # after first break before second break
+    elif time.hour < second_break_hour and time.hour >= start_hour:
         # standardize
-        if var_minutes > 30:
-            var_minutes = 30 - var_minutes
-        
+        if time.minute > 36:
+            var = second_break_hour - time.hour
+            var_minutes = 36 - time.minute
+            
             if var_minutes < 0:
                 var_minutes = 60 + var_minutes
-                var_hours -= 1
+                var -= 1
                 
             if var_minutes == 1:
-                print('It is', var_hours ,'hours and', var_minutes ,'minute until clock in')
+                print('It is', var ,'hours and', var_minutes ,'minute until lunch')
 
             else:
-                print('It is', var_hours ,'hours and', var_minutes ,'minutes until clock in')
+                print('It is', var ,'hours and', var_minutes ,'minutes until lunch')
+            
+        # no need to standardize 
         else:
-            var_minutes = 30 - var_minutes
+            var = second_break_hour - time.hour
+            var_minutes = 36 - time.minute
             
             if var_minutes == 1:
-                print('It is', var_hours ,'hours and', var_minutes ,'minute until clock in')
+                print('It is', var ,'hours and', var_minutes ,'minute until lunch')
 
             else:
-                print('It is', var_hours ,'hours and', var_minutes ,'minutes until clock in')
+                print('It is', var ,'hours and', var_minutes ,'minutes until lunch')
+                
+    elif time.hour == second_break_hour and time.minutes < 36:
+        var_minutes = 36 - time.minute
+        print('It is', var_minutes ,'minutes until lunch')
             
-    # between midnight and clock in
-    if time.hour >= 0 and time.hour < 6:
-        var_hours = time.hour
+    # it is currently lunch time
+    elif time.hour == second_break_hour and time.minutes >= 36:
+        print('It is lunch time :)')
+        
+    # after lunch before clock out
+    elif time.hour < end_hour and time.hour >= start_hour:
+        # standardize
+        if time.minute > 30:
+            var = end_hour - time.hour
+            var_minutes = 30 - time.minute
+            
+            if var_minutes < 0:
+                var_minutes = 60 + var_minutes
+                var -= 1
+                
+            if var_minutes == 1:
+                print('It is', var ,'hours and', var_minutes ,'minute until clock out')
+
+            else:
+                print('It is', var ,'hours and', var_minutes ,'minutes until clock out')
+            
+        # no need to standardize 
+        else:
+            var = end_hour - time.hour
+            var_minutes = 30 - time.minute
+            
+            if var_minutes == 1:
+                print('It is', var ,'hours and', var_minutes ,'minute until clock out')
+
+            else:
+                print('It is', var ,'hours and', var_minutes ,'minutes until clock out')
+            
+    # after lunch before clock out during end hour
+    elif time.hour == end_hour and time.minutes < 30:
         var_minutes = time.minute
+        print('It is', var_minutes ,'minutes until clock out')
+        
+    # outside of work hours
+    else:
+        during_work = False
+        print('You are not currently scheduled!')
+        # if it is before midnight
+        if time.hour >= start_hour and time.hour < 0:
+            # add 6 hours and 30 minutes onto difference between curr and midnight
+            var_hours = 24 - time.hour + start_hour
+            var_minutes = time.minutes
+            
+            # standardize
+            if var_minutes > 30:
+                var_minutes = 30 - var_minutes
+            
+                if var_minutes < 0:
+                    var_minutes = 60 + var_minutes
+                    var_hours -= 1
+                    
+                if var_minutes == 1:
+                    print('It is', var_hours ,'hours and', var_minutes ,'minute until clock in')
+
+                else:
+                    print('It is', var_hours ,'hours and', var_minutes ,'minutes until clock in')
+            else:
+                var_minutes = 30 - var_minutes
+                
+                if var_minutes == 1:
+                    print('It is', var_hours ,'hours and', var_minutes ,'minute until clock in')
+
+                else:
+                    print('It is', var_hours ,'hours and', var_minutes ,'minutes until clock in')
+                
+        # between midnight and clock in
+        if time.hour >= 0 and time.hour < start_hour:
+            var_hours = time.hour
+            var_minutes = time.minute
+            
+            # standardize
+            if var_minutes > 30:
+                var_minutes = 30 - var_minutes
+            
+                if var_minutes < 0:
+                    var_minutes = 60 + var_minutes
+                    var_hours -= 1
+                    
+                if var_minutes == 1:
+                    print('It is', var_hours ,'hours and', var_minutes ,'minute until clock in')
+
+                else:
+                    print('It is', var_hours ,'hours and', var_minutes ,'minutes until clock in')
+            else:
+                var_minutes = 30 - var_minutes
+                
+                if var_minutes == 1:
+                    print('It is', var_hours ,'hours and', var_minutes ,'minute until clock in')
+
+                else:
+                    print('It is', var_hours ,'hours and', var_minutes ,'minutes until clock in')
+                
+    # percentage of work day done
+    if during_work:
+        # divide current minutes by total minutes
+        total_minutes = 480
+        
+        curr_minutes = time.minute - 30
+        curr_hour = time.hour - start_hour
         
         # standardize
-        if var_minutes > 30:
-            var_minutes = 30 - var_minutes
-        
-            if var_minutes < 0:
-                var_minutes = 60 + var_minutes
-                var_hours -= 1
-                
-            if var_minutes == 1:
-                print('It is', var_hours ,'hours and', var_minutes ,'minute until clock in')
-
-            else:
-                print('It is', var_hours ,'hours and', var_minutes ,'minutes until clock in')
-        else:
-            var_minutes = 30 - var_minutes
+        if curr_minutes < 0:
+            curr_minutes = 60 + curr_minutes
+            curr_hour -= 1
             
-            if var_minutes == 1:
-                print('It is', var_hours ,'hours and', var_minutes ,'minute until clock in')
-
-            else:
-                print('It is', var_hours ,'hours and', var_minutes ,'minutes until clock in')
-            
-# percentage of work day done
-if during_work:
-    # divide current minutes by total minutes
-    total_minutes = 480
-    
-    curr_minutes = time.minute - 30
-    curr_hour = time.hour - 6
-    
-    # standardize
-    if curr_minutes < 0:
-        curr_minutes = 60 + curr_minutes
-        curr_hour -= 1
+        curr_minutes += 60 * curr_hour
         
-    curr_minutes += 60 * curr_hour
+        curr_minutes = (curr_minutes / total_minutes) * 100
+        curr_minutes = round(curr_minutes, 2)
+        
+        print('You are', curr_minutes,'% through the day')  
+        
+    # print current time
+    time = time.strftime('%H:%M:%S')
+    print(time)
     
-    curr_minutes = (curr_minutes / total_minutes) * 100
-    curr_minutes = round(curr_minutes, 2)
+def get_shift_name(shift):
+    if shift == '1':
+        return 'First'
     
-    print('You are', curr_minutes,'% through the day')  
-
-# print current time
-time = time.strftime('%H:%M:%S')
-print(time)
-
+    elif shift == '2':
+        return 'Second'
     
+    elif shift == '3':
+        return 'Third'
+    
+    else:
+        print('Invalid shift. Please run again')
+        quit()
+        
+    
+    
+def set_variables(shift):
+    # set hour variables according to the shift
+    global start_hour, first_break_hour, second_break_hour, end_hour, first_break_minute, second_break_minute
+    if shift == '1':
+        start_hour = 6
+        end_hour = 14
+        
+        first_break_hour = 8
+        second_break_hour = 11
+        
+        first_break_minute = 54
+        second_break_minute = 36
+        
+    if shift == '2':
+        start_hour = 14
+        first_break_hour = 16
+        second_break_hour = 19
+        first_break_minute = 54
+        second_break_minute = 36
+        end_hour = 22
+        
+    if shift == '3':
+        start_hour = 22
+        first_break_hour = 0
+        second_break_hour = 3
+        first_break_minute = 54
+        second_break_minute = 36
+        end_hour = 6
+    
+        
